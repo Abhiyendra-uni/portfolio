83867f7020430513b39b23acc3a9c02bc0012898
Abhiyendra-uni <abhiyendra.gahlot@griffithuni.edu.au>
2022-10-05 04:14:13 +1000

    first_comit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    
+    extend: {
+      colors:{
+        pin: 'hsl(333,100%,52%)',
+        bla: 'hsl(210deg, 30%, 8%)',
+        blu: 'hsl(230, 100%, 69%)',
+        softBlue: 'hsl(231, 69%, 60%)',
+        softRed: 'hsl(0, 94%, 66%)',
+        grayishBlue: 'hsl(229, 8%, 60%)',
+        veryDarkBlue: 'hsl(229, 31%, 21%)',
+      },
+      fontFamily:{
+        sans: ['PT Sans', 'sans-serif'],
+      },
+      backgroundImage: () => ({
+        dots: "url('../images/bg-dots.svg')",
+      }),
+    },
+  },
+  plugins: [require('tw-elements/dist/plugin')],
+}
